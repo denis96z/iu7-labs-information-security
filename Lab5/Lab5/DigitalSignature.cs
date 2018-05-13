63db981e32d0a50c049cea3966e1a434a064da20
denis96z <zinoviev.denis@yandex.ru>
2018-05-13 17:59:03 +0300

    Lab5 added.

---
+++ Lab5/Lab5/DigitalSignature.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace Lab5
+{
+    class DigitalSignature
+    {
+        private const string HASH_ALG_NAME = "SHA1";
+
+        public void CreateSignatureFile(string sourceFileName, string signatureFileName)
+        {
+            var hashAlgorithm = HashAlgorithm.Create(HASH_ALG_NAME);
+            var cryptoServiceProvider = new DSACryptoServiceProvider();
+
+            var signatureFormatter = new DSASignatureFormatter(cryptoServiceProvider);
+            signatureFormatter.SetHashAlgorithm(HASH_ALG_NAME);
+
+            var cspBlob = cryptoServiceProvider.ExportCspBlob(false);
+
+            var signFileStream = File.Create(signatureFileName);
+            using (var reader = File.OpenRead(sourceFileName))
+            using (var writer = new BinaryWriter(signFileStream))
+            {
+                var hash = hashAlgorithm.ComputeHash(reader);
+                var signature = signatureFormatter.CreateSignature(hash);
+
+                writer.Write(cspBlob);
+                writer.Write(signature);
+
+                reader.Close();
+                writer.Close();
+            }
+
+            hashAlgorithm.Dispose();
+            cryptoServiceProvider.Dispose();
+        }
+
+        public bool VerifySignature(string sourceFileName, string signatureFileName)
+        {
+            var hashAlgorithm = HashAlgorithm.Create(HASH_ALG_NAME);
+            var cryptoServiceProvider = new DSACryptoServiceProvider();
+
+            var signatureDeformatter = new DSASignatureDeformatter(cryptoServiceProvider);
+            signatureDeformatter.SetHashAlgorithm(HASH_ALG_NAME);
+
+            bool result = false;
+
+            var signatureStream = File.OpenRead(signatureFileName);
+            using (var sourceReader = File.OpenRead(sourceFileName))
+            using (var signatureReader = new BinaryReader(signatureStream))
+            {
+                var cspBlobLength = cryptoServiceProvider
+                    .ExportCspBlob(false).Length;
+                var cspBlob = signatureReader.ReadBytes(cspBlobLength);
+                cryptoServiceProvider.ImportCspBlob(cspBlob);
+
+                var hash = hashAlgorithm.ComputeHash(sourceReader);
+                var signatureLength = (int)(signatureReader.BaseStream.Length -
+                    signatureReader.BaseStream.Position);
+                var signature = signatureReader.ReadBytes(signatureLength);
+
+                result = signatureDeformatter.VerifySignature(hash, signature);
+
+                sourceReader.Close();
+                signatureReader.Close();
+            }
+
+            hashAlgorithm.Dispose();
+            cryptoServiceProvider.Dispose();
+
+            return result;
+        }
+    }
+}
---
+++ Lab5/Lab5/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Lab5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                if (args.Length == 1 && args[0] == "help")
+                {
+                    string exeName = Path
+                        .GetFileNameWithoutExtension(Application.ExecutablePath);
+
+                    Console.WriteLine("Создание подписи:");
+                    Console.WriteLine(exeName + " /s имя_файла -o имя_файла_подписи" + "\n");
+                    Console.WriteLine("Проверка подписи:");
+                    Console.WriteLine(exeName + " /v имя_файла -s имя_файла_подписи" + "\n");
+                }
+                else if (args.Length == 4)
+                {
+                    if (args[0].ToLower() == "/s")
+                    {
+                        string sourceFileName = args[1];
+                        FileInfo fileInfo = new FileInfo(sourceFileName);
+
+                        if (args[2] != "-o")
+                        {
+                            ThrowArgumentException(args[2]);
+                        }
+
+                        string signatureFileName = args[3];
+                        fileInfo = new FileInfo(signatureFileName);
+
+                        DigitalSignature digitalSignature = new DigitalSignature();
+                        digitalSignature.CreateSignatureFile(sourceFileName, signatureFileName);
+
+                        Console.WriteLine("Файл подписи создан.");
+                    }
+                    else if (args[0].ToLower() == "/v")
+                    {
+                        string sourceFileName = args[1];
+                        FileInfo fileInfo = new FileInfo(sourceFileName);
+
+                        if (args[2] != "-s")
+                        {
+                            ThrowArgumentException(args[2]);
+                        }
+
+                        string signatureFileName = args[3];
+                        fileInfo = new FileInfo(signatureFileName);
+
+                        DigitalSignature digitalSignature = new DigitalSignature();
+                        bool f = digitalSignature.VerifySignature(sourceFileName, signatureFileName);
+
+                        Console.WriteLine(f ? "Подпись действительна." : "Подпись недействительна.");
+                    }
+                    else
+                    {
+                        ThrowArgumentException(args[0]);
+                    }
+                }
+                else
+                {
+                    ThrowArgumentsNumberException();
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+        }
+
+        static void ThrowArgumentException(string arg)
+        {
+            throw new ArgumentException("Недопустимый параметр \"" + arg + "\".");
+        }
+
+        static void ThrowArgumentsNumberException()
+        {
+            throw new ArgumentException("Неверное число аргументов.");
+        }
+    }
+}
